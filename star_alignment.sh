7ae96932470c26604697d73742043fb16726a958
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-09-12 23:40:42 -0400

    First commit - STAR aligner
    Added the sbatch script for STAR alignment of fastq files

---
+++ star_alignment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --partition=PARTITION_NAME    # Partition (job queue)
+#SBATCH --requeue                     # Return job to the queue if preempted
+#SBATCH --job-name=STARMAP            # Assign a short name to your job
+#SBATCH --nodes=1                     # Number of nodes
+#SBATCH --ntasks=1                    # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=64            # Cores per task (>1 if multithread tasks)
+#SBATCH --mem=200G                    # Real memory (RAM) required (MB)
+#SBATCH --time=120:00:00              # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out      # STDOUT output file
+#SBATCH --error=slurm.%N.%j.err       # STDERR output file
+#SBATCH --export=ALL                  # Export current environment to job
+#SBATCH --mail-type=END,FAIL          # Send email on end or failure
+#SBATCH --mail-user=EMAIL_ID          # Where to send email
+
+
+ls *_1.fastq > list.txt
+sed -i 's/_1.fastq//g' list.txt
+
+
+GENOME_REF_DIR="/path/to/genomeDir"
+GTF_PATH="/path/to/annotation.gtf"
+
+
+while read -r line; do
+    STAR --genomeDir "${GENOME_REF_DIR}" \
+         --runThreadN ${SLURM_CPUS_PER_TASK} \
+         --readFilesIn "${line}_1.fastq" "${line}_2.fastq" \
+         --quantMode TranscriptomeSAM \
+         --sjdbGTFfile "${GTF_PATH}" \
+         --outFileNamePrefix "${line}_"
+done < list.txt
